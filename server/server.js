2feae10b23d1a637044b1d2e20f887784bdff8b6
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-02-02 20:11:38 +0000

    committing server.js because i could not create render webservice for server with empty file

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg"
+import dotenv from "dotenv"
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
